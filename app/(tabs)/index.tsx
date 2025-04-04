4dc2f7678e456cc4c795156ee2e69ff445708e03
ZeeAzeez <azeezahaazeez@gmail.com>
2025-04-04 08:22:00 +0100

    home page

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,17 +1,13 @@
-import { Text, View } from "react-native";
+import { Text, View, Image } from "react-native";
 import { Link } from "expo-router";
+import posts from '../../assets/data/posts.json';
 
 export default function Index() {
+  const post = posts[0];
   return (
-    <View
-      style={{
-        flex: 1,
-        justifyContent: "center",
-        alignItems: "center",
-      }}
-    >
-      <Text className="font-bold text-2xl my-10">Home Page</Text>
-      <Link href="/profile"> Profile Page</Link>
+    <View>
+      <Image source={{uri: post.group.image}} style={{width: 20, height: 20, borderRadius:10}}/>
+      <Text>{post.group.name}</Text>
     </View>
   );
 }
